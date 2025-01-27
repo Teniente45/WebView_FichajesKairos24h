52a6c7b0cd2b17e7be151a06a8efe648129a9ded
Teniente45 <Juanteniente45@gmail.com>
2025-01-27 10:44:44 +0100

    App perfecta

--- app/src/main/java/com/example/webview_fichajeskairos24h/KioskLauncherActivity.kt
+++ app/src/main/java/com/example/webview_fichajeskairos24h/KioskLauncherActivity.kt
@@ -1,3 +1,5 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.webview_fichajeskairos24h
 
 import android.annotation.SuppressLint
@@ -29,54 +31,63 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 internal class KioskLauncherActivity : AppCompatActivity() {
+
+    // Variables para gestionar el modo kiosko
     private var isKioskModeEnabled = false
     private var longPressStartTime: Long = 0
     private var isLongPress = false
-    private val longPressThreshold = 2000L // 2 segundos de duración para el largo presionado
+    private val longPressThreshold = 2000L // Tiempo en milisegundos para definir un largo presionado
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Configura la orientación de la pantalla como horizontal
+        // Configuración de la orientación de la pantalla como horizontal
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
 
-        // Oculta la interfaz del sistema para habilitar el modo de pantalla completa
+        // Ocultar la barra de navegación y otros elementos del sistema para pantalla completa
         hideSystemUI()
 
-        // Establece la aplicación como el lanzador predeterminado
+        // Establecer la app como lanzador predeterminado
         setAsDefaultLauncher()
 
-        // Habilita el modo kiosko
+        // Habilitar el modo kiosko
         enableKioskMode()
 
         // Configuración del WebView
         val webView = findViewById<WebView>(R.id.webView)
         val webSettings = webView.settings
+
+        // Habilitar JavaScript en el WebView
         webSettings.javaScriptEnabled = true
+        // Habilitar almacenamiento local (DOM Storage)
         webSettings.domStorageEnabled = true
+        // Deshabilitar caché para asegurar que la página siempre se cargue desde la red
         webSettings.cacheMode = WebSettings.LOAD_NO_CACHE
 
-        // Limpia caché, historial y cookies
+        // Limpiar caché, historial y cookies al iniciar
         webView.clearCache(true)
         webView.clearHistory()
         webView.clearFormData()
         CookieManager.getInstance().removeAllCookies(null)
         CookieManager.getInstance().flush()
 
-        // Configuración del cliente para manejo de eventos y errores en el WebView
+        // Configuración del cliente WebViewClient para manejar eventos de página y errores
         webView.webViewClient = object : WebViewClient() {
             override fun onPageFinished(view: WebView, url: String) {
                 super.onPageFinished(view, url)
+                // Verificar que la URL cargada es válida
                 if (url.contains("kairos24h.es") && URLUtil.isValidUrl(url)) {
                     Log.d("KioskLauncherActivity", "URL final kairos y válida: $url")
                 } else {
+                    // Mostrar un mensaje si la URL no es válida
                     Toast.makeText(this@KioskLauncherActivity, "URL no válida", Toast.LENGTH_LONG)
                         .show()
                 }
             }
 
+            // Manejo de errores al cargar recursos en el WebView
             override fun onReceivedError(
                 view: WebView,
                 request: WebResourceRequest,
@@ -85,27 +96,22 @@ internal class KioskLauncherActivity : AppCompatActivity() {
                 super.onReceivedError(view, request, error)
                 val urlError = request.url.toString()
                 val errorMessage = error.description.toString()
-                Log.e(
-                    "WebViewError",
-                    "Error cargando URL: $urlError | Error: $errorMessage"
-                )
+                Log.e("WebViewError", "Error cargando URL: $urlError | Error: $errorMessage")
             }
         }
 
-        // Interfaz de JavaScript para interactuar con la app desde el WebView
+        // Añadir la interfaz JavaScript para poder interactuar con la app desde el WebView
         webView.addJavascriptInterface(
-            WebAppInterface(
-                this
-            ), "Android"
+            WebAppInterface(this), "Android"
         )
 
-        // Carga la URL inicial
-        val url =
-            "https://setfichaje.kairos24h.es/index.php?r=citaRedWeb/cppIndex&xEntidad=1003&cKiosko=TABLET1"
+        // Cargar la URL de la página inicial
+        val url = "https://setfichaje.kairos24h.es/index.php?r=citaRedWeb/cppIndex&xEntidad=1003&cKiosko=TABLET1"
         webView.loadUrl(url)
     }
 
-    // Habilita el modo kiosko, configurando restricciones de navegación y accesos
+    // Función para habilitar el modo kiosko
+    @SuppressLint("ObsoleteSdkInt")
     private fun enableKioskMode() {
         val dpm = getSystemService(DEVICE_POLICY_SERVICE) as DevicePolicyManager
         val adminName = ComponentName(this, AdminReceiver::class.java)
@@ -115,6 +121,7 @@ internal class KioskLauncherActivity : AppCompatActivity() {
                 // Configura la app como la única anclada en modo kiosko
                 dpm.setLockTaskPackages(adminName, arrayOf(packageName))
 
+                // Dependiendo de la versión del sistema, habilitar restricciones de tarea
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                     dpm.setLockTaskFeatures(adminName, DevicePolicyManager.LOCK_TASK_FEATURE_NONE)
                 } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -124,46 +131,36 @@ internal class KioskLauncherActivity : AppCompatActivity() {
                     )
                 }
 
-                // Activa el modo kiosko
+                // Activar el modo kiosko
                 startLockTask()
                 isKioskModeEnabled = true
-                Toast.makeText(this, "Modo kiosko activado. App anclada.", Toast.LENGTH_SHORT)
-                    .show()
+                Toast.makeText(this, "Modo kiosko activado. App anclada.", Toast.LENGTH_SHORT).show()
             } else {
                 Log.e("KioskLauncherActivity", "La app NO es Device Owner.")
-                Toast.makeText(
-                    this,
-                    "No somos Device Owner. No se puede habilitar el modo kiosko.",
-                    Toast.LENGTH_LONG
-                ).show()
+                Toast.makeText(this, "No somos Device Owner. No se puede habilitar el modo kiosko.", Toast.LENGTH_LONG).show()
             }
         } catch (e: Exception) {
             Log.e("KioskLauncherActivity", "Error al habilitar modo kiosko: " + e.message)
-            Toast.makeText(this, "Error al habilitar el modo kiosko.", Toast.LENGTH_SHORT).show()
         }
     }
 
-    // Deshabilita el modo kiosko y permite salir del entorno restringido
+    // Función para deshabilitar el modo kiosko
     private fun disableKioskMode() {
         try {
             stopLockTask()
             isKioskModeEnabled = false
-            Toast.makeText(
-                this,
-                "Modo kiosko desactivado. Navegación habilitada.",
-                Toast.LENGTH_SHORT
-            ).show()
         } catch (e: Exception) {
             Log.e("KioskLauncherActivity", "Error al detener LockTask: " + e.message)
         }
     }
 
-    // Establece la aplicación como el lanzador predeterminado del dispositivo
+    // Establece la aplicación como el lanzador predeterminado
     private fun setAsDefaultLauncher() {
         val dpm = getSystemService(DEVICE_POLICY_SERVICE) as DevicePolicyManager
         val admin = ComponentName(this, AdminReceiver::class.java)
 
         if (dpm.isDeviceOwnerApp(packageName)) {
+            // Filtrar las actividades para establecer la app como lanzador predeterminado
             val filter = IntentFilter(Intent.ACTION_MAIN)
             filter.addCategory(Intent.CATEGORY_HOME)
             filter.addCategory(Intent.CATEGORY_DEFAULT)
@@ -173,16 +170,17 @@ internal class KioskLauncherActivity : AppCompatActivity() {
         }
     }
 
-    // Oculta la barra de navegación y otros elementos del sistema para el modo de pantalla completa
+    // Función para ocultar las barras del sistema y lograr pantalla completa
+    @SuppressLint("ObsoleteSdkInt")
     private fun hideSystemUI() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             val controller = window.insetsController
             if (controller != null) {
                 controller.hide(WindowInsets.Type.systemBars())
-                controller.systemBarsBehavior =
-                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             }
         } else {
+            // Para versiones anteriores, usar flags del sistema
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
                     View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
                     View.SYSTEM_UI_FLAG_FULLSCREEN or
@@ -192,7 +190,7 @@ internal class KioskLauncherActivity : AppCompatActivity() {
         }
     }
 
-    // Alterna entre habilitar y deshabilitar el modo kiosko
+    // Función para alternar entre activar y desactivar el modo kiosko
     private fun toggleKioskMode() {
         if (isKioskModeEnabled) {
             disableKioskMode()
@@ -206,10 +204,13 @@ internal class KioskLauncherActivity : AppCompatActivity() {
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
         if (ev == null) return super.dispatchTouchEvent(ev)
 
+        // Obtener el ancho de la pantalla
+        val screenWidth = resources.displayMetrics.widthPixels
+
         when (ev.action) {
             MotionEvent.ACTION_DOWN -> {
-                val screenHeight = resources.displayMetrics.heightPixels
-                if (ev.y < screenHeight / 4) {
+                // Detectar si el toque está en los dos cuartos derechos de la pantalla
+                if (ev.x > screenWidth / 2) {
                     longPressStartTime = System.currentTimeMillis()
                     isLongPress = true
                 } else {
@@ -241,6 +242,7 @@ internal class KioskLauncherActivity : AppCompatActivity() {
             .setView(editText)
             .setPositiveButton("Aceptar") { dialogInterface: DialogInterface, i: Int ->
                 val pinIngresado = editText.text.toString().trim { it <= ' ' }
+                // Verificar si el PIN es correcto
                 if (checkPin(pinIngresado)) {
                     disableKioskMode()
                     clearDefaultLauncher()
@@ -283,16 +285,10 @@ internal class KioskLauncherActivity : AppCompatActivity() {
                 dpm.clearPackagePersistentPreferredActivities(admin, packageName)
                 Toast.makeText(this, "Lanzador predeterminado eliminado", Toast.LENGTH_SHORT).show()
             } catch (e: Exception) {
-                Log.e(
-                    "KioskLauncherActivity",
-                    "Error al limpiar el lanzador predeterminado: " + e.message
-                )
+                Log.e("KioskLauncherActivity", "Error al limpiar el lanzador predeterminado: " + e.message)
             }
         } else {
-            Log.e(
-                "KioskLauncherActivity",
-                "No somos Device Owner, no se puede eliminar el lanzador."
-            )
+            Log.e("KioskLauncherActivity", "No somos Device Owner, no se puede eliminar el lanzador.")
         }
     }
 
@@ -308,41 +304,16 @@ internal class KioskLauncherActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         hideSystemUI()
-        if (!isKioskModeEnabled) {
-            enableKioskMode()
-        }
-    }
-
-    // Intenta reactivar el modo kiosko si está habilitado y la actividad se pausa
-    override fun onPause() {
-        super.onPause()
-        try {
-            if (isKioskModeEnabled) {
-                enableKioskMode()
-            }
-        } catch (e: Exception) {
-            Log.e("KioskLauncherActivity", "Error en onPause: " + e.message)
-        }
-    }
-
-    // Bloquea el botón "Back" en modo kiosko
-    override fun onBackPressed() {
-        if (isKioskModeEnabled) {
-            Toast.makeText(
-                this,
-                "El botón Back está desactivado en modo kiosko",
-                Toast.LENGTH_SHORT
-            ).show()
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    // Clase interna para la interfaz con JavaScript
-    internal inner class WebAppInterface(private val context: Context) {
-        @JavascriptInterface
-        fun showToast(message: String?) {
-            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
-        }
     }
 }
+
+class WebAppInterface(private val context: Context) {
+
+    // Método accesible desde JavaScript
+    @JavascriptInterface
+    fun showToast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+}
+
+
