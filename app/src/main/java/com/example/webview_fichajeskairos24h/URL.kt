eee5250a04fcc436b16b27c7c800a90c7a5d8ce3
Teniente45 <Juanteniente45@gmail.com>
2025-01-27 13:21:20 +0100

    Varias const en URL App comppletada(de momento)

--- app/src/main/java/com/example/webview_fichajeskairos24h/URL.kt
+++ app/src/main/java/com/example/webview_fichajeskairos24h/URL.kt
@@ -1,8 +1,8 @@
 package com.example.webview_fichajeskairos24h
 
 object WebViewURL {
-    const val DEMO = "http://192.168.25.47:8008/cppTablet/index.php?r=citaredweb/cppIndex&xEntidad=1003&cKiosko=TABLET1"
-    const val PRODUCCION = "https://setfichaje.kairos24h.es/index.php?r=citaRedWeb/cppIndex&xEntidad=1003&cKiosko=TABLET1"
+    const val DEMO = "https://setfichaje.kairos24h.es/index.php?r=citaRedWeb/cppIndex&xEntidad=1003&cKiosko=TABLET1"
+    const val PRODUCCION = ""
     // La URL de la página inicial
     const val LOGIN_URL = DEMO
 }
